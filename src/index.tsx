c7fc2be65839f4a308b619200233e836782f7e1d
winter limelight <winter_limelight@yahoo.com.au>
2018-01-21 13:29:44 +1300

    Routing and nav

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,44 @@
+
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { HashRouter, Route, NavLink } from "react-router-dom"
+
+import { ChartPage } from "./components/ChartPage";
 import "normalize.css"
 import "./css/global.scss";
-import { ChartPage } from "./components/ChartPage";
 
-ReactDOM.render(
+const homePage = () => 
+    <div>
+        <h1>React-App-2</h1>
+        Welcome. Please select a page from the navigation menu above.
+    </div>
+
+const dataPage = () =>
+    <div>
+        <h1>Data Entry</h1>
+        TODO
+    </div>
+
+const chartPage = () => 
     <div>
         <h1>Flight Passenger Totals</h1>
         <ChartPage />
-    </div>,
+    </div>
+
+
+ReactDOM.render(
+    <HashRouter>
+        <div>
+            <nav>
+                <NavLink exact to="/">Home</NavLink>
+                <NavLink to="/data">Data</NavLink>
+                <NavLink to="/charts">Charts</NavLink>
+            </nav>
+
+            <Route exact path="/" component={homePage} />
+            <Route path="/data" component={dataPage} />
+            <Route path="/charts" component={chartPage} />
+        </div>
+    </HashRouter>,
     document.getElementById("example")
 );
